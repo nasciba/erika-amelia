a412171363d2815cae101849c6cf32e32abeb44f
Amanda <fulanodetal@exemplo.br>
2026-03-03 12:46:17 -0300

    delete unused code

--- nextjs-erika-amelia/src/app/page.tsx
+++ nextjs-erika-amelia/src/app/page.tsx
@@ -8,26 +8,7 @@ export default function HomePage() {
   return (
     <BackgroundImage>
       <SectionsWrap>
-        <PageSection></PageSection>
-
-        {/* <CardGrid>
-        <Card href="/bio">
-          <CardTitle>Bio</CardTitle>
-          <CardDescription>
-            Learn about the artist and her practice.
-          </CardDescription>
-        </Card>
-        <Card href="/work">
-          <CardTitle>Work</CardTitle>
-          <CardDescription>Browse work by category.</CardDescription>
-        </Card>
-        <Card href="/contact">
-          <CardTitle>Contact</CardTitle>
-          <CardDescription>
-            Get in touch for commissions and inquiries.
-          </CardDescription>
-        </Card>
-      </CardGrid> */}
+        <PageSection />
       </SectionsWrap>
     </BackgroundImage>
   );
